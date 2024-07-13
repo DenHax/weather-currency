185206a9fd2ba3926c024655496b2c1cf41c3248
DenHax <whatxpw@gmail.com>
2024-07-13 22:59:27 +0300

    Добавлен скелет страницы погоды в компоненте Weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import CurrencyRateChart from './Components/Currency';
+import WeatherForecast from './Components/Weather';
 
 function App() {
   return (
     <div className='API'>
       <header className='API-header'>
         <h1>Курс доллара и прогноз погоды</h1>
+        <WeatherForecast />
         <CurrencyRateChart />
       </header>
     </div>
---
+++ src/Components/Weather.tsx
@@ -0,0 +1,27 @@
+//import axios from 'axios'
+//import { saver } from 'file-saver'
+//import { Line } from 'react-chartjs-2'
+//import 'chart.js/auto'
+//import { useEffect } from 'react'
+
+
+const WeatherForecast = () => {
+
+  //useEffect(() => {
+  //  axios.get()
+  //})
+  return (
+    <div>
+      <h2>Прогноз погоды на неделю</h2>
+      <div>
+        <h3>Удаление пунктов из прогноза</h3>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+
+}
+
+export default WeatherForecast;

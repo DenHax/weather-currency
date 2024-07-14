45478972e8bbe507748ffb42db354f6446323cc6
DenHax <whatxpw@gmail.com>
2024-07-14 07:09:24 +0300

    Добавлены роутеры для страницы погоды и курса валюты. Добавлена заглавная страница. Проблема - некрасивые стили

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import './App.css'
 import CurrencyRateChart from './Components/Currency';
+import Homepage from './Components/Homepage';
 import WeatherForecast from './Components/Weather';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className='API'>
       <header className='API-header'>
-        <h1>Курс доллара и прогноз погоды</h1>
-        <WeatherForecast />
-        <CurrencyRateChart />
+        <Link to="/">Дом</Link>
+        <Link to="/rate">Доллар</Link>
+        <Link to="/forecast">Погода</Link>
       </header>
+      <main>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/rate' element={<CurrencyRateChart />} />
+          <Route path='/forecast' element={<WeatherForecast />} />
+          <Route path='*' element={<Homepage />} />
+        </Routes>
+      </main>
     </div>
   )
 }
